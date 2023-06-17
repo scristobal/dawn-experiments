6018c2ee93c2242f10a3327a1e31014bcd60d22b
Samuel Cristobal <el.tobal@gmail.com>
2023-06-17 18:58:35 +0200

    empty window with glfw

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 
-int main(int, char **)
-{
+int main(int, char**) {
+    if (!glfwInit()) {
+        std::cerr << "Could not initialize GLFW!" << std::endl;
+        return 1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Learn WebGPU", NULL, NULL);
+
+    if (!window) {
+        std::cerr << "Could not open window!" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();  // waits for events
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
     std::cout << "Hello, world!" << std::endl;
     return 0;
 }
